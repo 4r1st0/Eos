fa38678fbe9cecd6f468a4f1b43d661090b07dbd
M. Dommert <dommer01@N13223.PAD.PTB.DE>
2016-11-24 13:50:16 +0100

    updated description - 2017-11-24

--- src/eos/io/OutputFormatter.java
+++ src/eos/io/OutputFormatter.java
@@ -11,13 +11,17 @@ import java.text.*;
  *
  * @author Martin Dommert, 2016-11-23
  * 
- * The OutputFormatter class provides the exponentSign method. This method is
+ * Utility class for output data formatting.
+ * OutputFormatter class provides the exponentSign method. This method is
  * used to extend the DecimalFormat method to provide + and - exponent signs.
- * DecimalFormat method: 0.0000E00      -> 1.2345E01 and 1.2345E-01
- * exponentSign method:  0.0000E(+/-)00 -> 1.2345E+01 and 1.2345E-01
- * 
- * The special format can be used for UMG data input files, which requires equal
- * length of data values.
+ * standard DecimalFormat method: take any double and convert to 0.0000E00
+ *                                12345     ->  1.2345E04 and 
+ *                                0.12341   ->  1.2340E-01
+ * exponentSign method:           take any double and convert to 0.0000E(+/-)00
+ *                                12345     ->  1.2345E+04 and
+ *                                0.12341   ->  1.2340E-01
+ * The special  exponentSign format can be used for UMG data input files,
+ * which requires equal length of data values.
  */
 public class OutputFormatter {
     
