94b2ff3b4f994e91adaa4f1a0693ca45de3a9b79
M. Dommert <dommer01@N13223.PAD.PTB.DE>
2016-11-28 16:55:33 +0100

    add: new Log-TextArea. Check for correct input - 2017-11-28

--- src/eos/gui/defaultSpectrumGUI.java
+++ src/eos/gui/defaultSpectrumGUI.java
@@ -25,6 +25,7 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
     private int numberOfEnergyBins;
     private int spectrumForm;
     private int energyScale;
+    private int errorFlag = 0;
     private double energyHighBinEdge;
     private List<Double> energyLeftEdge = new ArrayList<Double>();
     private List<Double> spectrumBin = new ArrayList<Double>();
@@ -85,6 +86,7 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTextFieldOutName = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
+        textAreaOutLog = new java.awt.TextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(620, 920));
@@ -282,18 +284,24 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
 
         jLabel10.setText(".flu");
 
+        textAreaOutLog.setFont(new java.awt.Font("Dialog", 0, 10)); // NOI18N
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextFieldOutName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(2, 2, 2)
+                        .addComponent(jLabel10)
+                        .addGap(0, 44, Short.MAX_VALUE))
+                    .addComponent(textAreaOutLog, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldOutName, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel10)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButtonCreateDefaultSpectrum, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButtonClearAll, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -308,31 +316,32 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jTextFieldOutName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel10))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButtonClearSpecUnc)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
-                .addComponent(jButtonClearAll)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jButtonClearSpecUnc)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonClearAll))
+                    .addComponent(textAreaOutLog, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(39, 39, 39))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(19, 19, 19))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -367,9 +376,16 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
         
         try {
             numberOfEnergyBins = Integer.parseInt(jTextFieldEnergyBins.getText());
+        } catch (NumberFormatException e) {
+            textAreaOutLog.append("# of Energy Bins is no integer!\n");
+            errorFlag = 1;
+        }
+        
+        try {
             energyHighBinEdge = Double.parseDouble(jTextField4.getText());
         } catch (NumberFormatException e) {
-        // Invalid double String.
+            textAreaOutLog.append("Highest energy bin edge is no double variable!\n");
+            errorFlag = 1;
         }
         
         for (String line : jTextAreaBinEdge.getText().split("\\n")) {
@@ -384,19 +400,24 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
         //============END: Read data from GUI===================================
 
         //============START: Write data to file=================================
-        try{
-            WriteDefaultSpectrum writeDefaultSpec = new WriteDefaultSpectrum();
-            writeDefaultSpec.WriteDefaultSpectrum(jTextFieldOutName,
-                                     headerSpectrum, numberOfEnergyBins,
-                                     spectrumForm, energyScale,
-                                     energyHighBinEdge,
-                                     energyLeftEdge,
-                                     spectrumBin,
-                                     spectrumBinUncertainty);
-        } catch (Exception e) {
-            System.out.println("problem with path : "+ jTextFieldOutName.getText());
-            System.out.println(e.getMessage());
+        if(errorFlag != 0) {
+            textAreaOutLog.append("Input error. Please check all input fields!\n\n");
+        } else {
+            try{
+                WriteDefaultSpectrum writeDefaultSpec = new WriteDefaultSpectrum();
+                writeDefaultSpec.WriteDefaultSpectrum(jTextFieldOutName,
+                                         headerSpectrum, numberOfEnergyBins,
+                                         spectrumForm, energyScale,
+                                         energyHighBinEdge,
+                                         energyLeftEdge,
+                                         spectrumBin,
+                                         spectrumBinUncertainty);
+            } catch (Exception e) {
+                System.out.println("problem with path : "+ jTextFieldOutName.getText());
+                System.out.println(e.getMessage());
+            }
         }
+        
      
 
         //============END: Write data to file===================================
@@ -505,5 +526,6 @@ public class defaultSpectrumGUI extends javax.swing.JFrame {
     private javax.swing.JTextField jTextFieldEnergyBins;
     private javax.swing.JTextField jTextFieldHeaderSpectrum;
     private javax.swing.JTextField jTextFieldOutName;
+    private java.awt.TextArea textAreaOutLog;
     // End of variables declaration//GEN-END:variables
 }
